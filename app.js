e7fb0bc90cbb5a0251a752a369a2935c84731e1e
Kurt Kroeker <kurt.kroeker@energycap.com>
2019-01-25 15:26:05 -0500

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+// var express=require('express');
+// var addition = require('./Addition.js');
+var tutor = require('./NodeTutorial.js');
+
+// var app=express();
+
+// app.set('view engine','jade');
+
+// app.get('/',function(req,res)
+// {    
+// });
+
+// var server=app.listen(3000,function()
+// {
+// });
+
+// var result = addition.AddNumber(1, 2);
+// console.log(result);
+
+var blah = new tutor.NodeTutorial;
+
+blah.pTutor();
